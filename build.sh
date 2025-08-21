70edd42c2ca25a28a49da3bae8651205556f8ce5
evilbinary <rootntsd@gmail.com>
2025-08-22 00:57:20 +0800

    add image

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
-gcc main.c -I/usr/local/Cellar/cjson/1.7.17/include/cjson -lcjson -I/opt/local/include/ -L/opt/local/lib -lSDL2 -lSDL2_ttf  -o main
+#gcc main.c -I/usr/local/Cellar/cjson/1.7.17/include/cjson -lcjson -I/opt/local/include/ -L/opt/local/lib -lSDL2 -lSDL2_ttf -lSDL_image  -o main
+
+gcc main.c -I/usr/local/Cellar/cjson/1.7.17/include/cjson -lcjson \
+-F../libs/ -framework SDL2  -framework SDL2_ttf  -framework SDL2_image  \
+ -I../libs/SDL2.framework/Headers \
+ -I../libs/SDL2_ttf.framework/Headers \
+ -I../libs/SDL2_image.framework/Headers \
+-o main
