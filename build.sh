e3a676206f83b83c663d8314d06cbb97ab0939b3
evilbinary <rootntsd@gmail.com>
2025-08-23 15:02:06 +0800

    update res

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #gcc main.c -I/usr/local/Cellar/cjson/1.7.17/include/cjson -lcjson -I/opt/local/include/ -L/opt/local/lib -lSDL2 -lSDL2_ttf -lSDL_image  -o main
 
-gcc main.c -I/usr/local/Cellar/cjson/1.7.17/include/cjson -lcjson \
+gcc -g main.c -I/usr/local/Cellar/cjson/1.7.17/include/cjson -lcjson \
 -F../libs/ -framework SDL2  -framework SDL2_ttf  -framework SDL2_image  \
  -I../libs/SDL2.framework/Headers \
  -I../libs/SDL2_ttf.framework/Headers \
