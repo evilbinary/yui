0eceda56b3744d2a556a6c5a60fb80b8a4f2e6df
evilbinary <rootntsd@gmail.com>
2026-01-02 22:24:14 +0800

    feat: 添加yui静态库构建配置

---
+++ src/ya.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+# *******************************************************************
+# * Copyright 2025-present evilbinary
+# * 作者: evilbinary on 01/20/2025
+# * 邮箱: rootntsd@gmail.com
+# ********************************************************************
+
+target('yui')
+add_deps("cjson")
+add_flags()
+set_kind('static')
+add_files("src/*.c")
+add_files("src/components/*.c")
