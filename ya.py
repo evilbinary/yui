c290a09ab66aa7928e42248b4817226330e4d5cc
evilbinary <rootntsd@gmail.com>
2026-02-13 23:38:55 +0800

    feat: 添加Emscripten平台支持并重构编译选项

--- ya.py
+++ ya.py
@@ -47,25 +47,104 @@ def add_flags():
             '-fsanitize=address',
         )
 
-    if platform.system()=='Darwin':
-        add_cflags(
+    if get_plat() in['emscripten','em']:
+        set_toolchain('emscripten')
+        # tool=get_toolchain_node()
+        # tool['cc']='emcc'
+        # tool['cxx']='emcc'
+        # tool['ld']='emcc'
+        # tool['ar']='emar'
+        if platform.system()=='Windows':
+            mingw64='E:\\soft\\msys2\\mingw64'
+            add_cflags(
+            # '--use-port=sdl2 ',
             '-g',
+            '-Wno-incompatible-pointer-types -Wno-implicit-function-declaration',
             '-F../libs/',
-            '-I../libs/SDL2.framework/Headers',
-            '-I../libs/SDL2_ttf.framework/Headers',
-            '-I../libs/SDL2_image.framework/Headers',
-            '-Isrc',
-            '-Ilib',
-            '-Ilib/mquickjs',
-            '-DHAS_JS_MODULE'
-            ),
-        add_ldflags(
+            '-I'+mingw64+'\\include\\SDL2',
+            '-I.',
+            '-I./src/components',
+            '-I./src/'
+            )
+            add_ldflags(
+            
+            '-s USE_SDL=2',
+            '-s USE_SDL_IMAGE=2',
+            '-s USE_SDL_TTF=2',
+            '-s ALLOW_MEMORY_GROWTH=1',
+            '-s ASSERTIONS=1',
+            #'-s EXPORTED_FUNCTIONS=["_main"]',
+            #'-s EXPORTED_RUNTIME_METHODS=["ccall","cwrap","FS","stringToUTF8","UTF8ToString"]',
+            '-s EXPORT_ALL=1 ',
+            '-s ASSERTIONS=2 ',
+            '-s STACK_OVERFLOW_CHECK=2',
+            '-g4',
+            '-O0',
+            '-Wbad-function-cast ',
+            '-Wcast-function-type',
+            '--source-map-base http://localhost:6931/',
+
+            '-s ERROR_ON_UNDEFINED_SYMBOLS=0',
+            '-s AGGRESSIVE_VARIABLE_ELIMINATION=1',
+            '--preload-file app/playground/',
+            '--preload-file app/assets/',
+            '--preload-file app/lib/',
+            '--preload-file app',
+            '-L'+mingw64+'\\lib',
+            '-lSDL2main',
+            '-lSDL2',
+            '-lSDL2_ttf',
+            '-lSDL2_image',
+            '-lm'
+            )
+        else:
+            mingw64='../libs/'
+
+            add_cflags(
+            # '--use-port=sdl2 ',
+            '-g',
+            '-Wno-incompatible-pointer-types -Wno-implicit-function-declaration',
             '-F../libs/',
-        '-framework SDL2',
-            '-framework SDL2_ttf',
-            '-framework SDL2_image ',
-            '-F../libs/'
-            ),
+            '-I'+mingw64+'\\include\\SDL2',
+            '-I.',
+            '-I./src/components',
+            '-I./src/',
+            '-s USE_SDL=2',
+            '-s USE_SDL_IMAGE=2',
+            '-s USE_SDL_TTF=2',
+            )
+        
+            add_ldflags(
+                
+                '-s USE_SDL=2',
+                '-s USE_SDL_IMAGE=2',
+                '-s USE_SDL_TTF=2',
+                '-s ALLOW_MEMORY_GROWTH=1',
+                '-s ASSERTIONS=1',
+                #'-s EXPORTED_FUNCTIONS=["_main"]',
+                #'-s EXPORTED_RUNTIME_METHODS=["ccall","cwrap","FS","stringToUTF8","UTF8ToString"]',
+                '-s EXPORT_ALL=1 ',
+                '-s ASSERTIONS=2 ',
+                '-s STACK_OVERFLOW_CHECK=2',
+                '-g4',
+                '-O0',
+                '-Wbad-function-cast ',
+                '-Wcast-function-type',
+                '--source-map-base http://localhost:6931/',
+
+                '-s ERROR_ON_UNDEFINED_SYMBOLS=0',
+                '-s AGGRESSIVE_VARIABLE_ELIMINATION=1',
+                '--preload-file app/playground/',
+                '--preload-file app/assets/',
+                '--preload-file app/lib/',
+                '--preload-file app',
+                '-L'+mingw64+'\\lib',
+               
+                '-lSDL2',
+                '-lSDL2_ttf',
+                '-lSDL2_image',
+                '-lm'
+                )
     elif is_plat("stm32"):
         # STM32平台特定的编译选项
         add_cflags(
@@ -104,6 +183,25 @@ def add_flags():
             '-lstdc++',
             '-lsupc++',
             '-Wl,--end-group'
+            ),
+    elif platform.system()=='Darwin':
+        add_cflags(
+            '-g',
+            '-F../libs/',
+            '-I../libs/SDL2.framework/Headers',
+            '-I../libs/SDL2_ttf.framework/Headers',
+            '-I../libs/SDL2_image.framework/Headers',
+            '-Isrc',
+            '-Ilib',
+            '-Ilib/mquickjs',
+            '-DHAS_JS_MODULE'
+            ),
+        add_ldflags(
+            '-F../libs/',
+        '-framework SDL2',
+            '-framework SDL2_ttf',
+            '-framework SDL2_image ',
+            '-F../libs/'
             ),
     elif platform.system()=='Linux':
         add_cflags(
