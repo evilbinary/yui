3dd88ac1b5a69334a2fef4258cbf14caea3dff10
evilbinary <rootntsd@gmail.com>
2026-07-20 17:11:18 +0800

    refactor(backend): rename mouse state function to pointer state for improved clarity and consistency

--- src/backend.h
+++ src/backend.h
@@ -26,7 +26,7 @@ void backend_quit();
 void backend_render_present();
 void backend_delay(int delay);
 Uint32 backend_get_ticks(void);
-void backend_get_mouse_state(int* x, int* y);
+void backend_get_pointer_state(int* x, int* y);
 void backend_render_clear_color(unsigned char r,unsigned char g,unsigned char b,unsigned char a);
 
 DFont* backend_load_font(char* font_path,int size);
